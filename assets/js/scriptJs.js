d61dfc6de7b64412fd95a6c73bfe548cde2aa163
KAUA MOREIRA DA SILVA <kaua.msilva16@senacsp.edu.br>
2026-06-11 17:23:08 -0300

    ajuste: nas imagens, efeitos scroll e limpeza de arquivo

--- assets/js/scriptJs.js
+++ assets/js/scriptJs.js
@@ -1,23 +1,50 @@
-const whatsappBtn = document.getElementById('whatsapp-btn');
+const whatsappBtn = document.querySelectorAll('.whatsapp-btn');
 
-whatsappBtn.addEventListener('click', () => {
-    const phoneNumber = '55991999999'; // Substitua pelo número de telefone desejado
+whatsappBtn.forEach(btn => {
+    btn.addEventListener('click', () => {
+        const phoneNumber = '55991999999'; // Substitua pelo número de telefone desejado
 
-    const horaAtual = new Date().getHours();
+        const horaAtual = new Date().getHours();
 
-    let saudacao;
+        let saudacao;
 
-    if (horaAtual < 12){
-        saudacao = 'Bom dia';
-    }else if (horaAtual < 18){
-        saudacao = 'Boa tarde';
-    }else{
-        saudacao = 'Boa noite';
-    }
+        if (horaAtual < 12) {
+            saudacao = 'Bom dia';
+        } else if (horaAtual < 18) {
+            saudacao = 'Boa tarde';
+        } else {
+            saudacao = 'Boa noite';
+        }
 
-    const mensagem = `Olá, ${saudacao}! Gostaria de saber mais sobre o link de indicação?`;
-   
-    const link = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(mensagem)}`;
+        const mensagem = `Olá, ${saudacao}! Gostaria de saber mais sobre o link de indicação?`;
 
-    window.open(link, "_blank");
+        const link = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(mensagem)}`;
+
+        window.open(link, "_blank");
+    });
+
+});
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.utils.toArray("section").forEach((section) => {
+
+    const elementos = section.querySelectorAll(
+        "h2, h3, p, img, .btn-style-two .btn-wrap"
+    );
+
+    gsap.from(elementos, {
+        y: 80,
+        opacity: 0,
+        duration: 1,
+        stagger: 0.15,
+        ease: "power3.out",
+
+        scrollTrigger: {
+            trigger: section,
+            start: "top 80%"
+        }
+    });
+
 });
